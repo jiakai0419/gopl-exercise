c57a05a64aafc2c923911d9165f1d88b9467103a
jiakai <jiakai@bytedance.com>
2017-08-27 15:59:57 +0800

    exercise 2.2 - 2.5
    Change-Id: I15717e9a0415dcc0d0f8bb1e6bd0e4498b411890

---
+++ exercise-2.3/popcount/exercise-2.3_test.go
@@ -0,0 +1,22 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+package popcount
+
+import (
+	"testing"
+)
+
+// -- Benchmarks --
+
+func BenchmarkPopCount(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCount(0x1234567890ABCDEF)
+	}
+}
+
+func BenchmarkPopCountLoop(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCountLoop(0x1234567890ABCDEF)
+	}
+}
